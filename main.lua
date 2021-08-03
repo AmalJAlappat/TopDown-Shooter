b28d601e40383ec21c8be01c3243133a9ab8b09d
Amal-J-Alappat <amalalappattofficial@gmail.com>
2021-08-03 20:18:16 +0530

    Final Version

--- main.lua
+++ main.lua
@@ -9,6 +9,8 @@ function love.load()
     player.x=love.graphics.getWidth()/2
     player.y=love.graphics.getHeight()/2
     player.speed=180
+    player.injured=false
+    player.injuredSpeed=270
 
     screenHeight = love.graphics.getHeight()
     screenWidth = love.graphics.getWidth()
@@ -23,6 +25,8 @@ function love.load()
 
     score=0
     highscore=0
+    
+    
 
     myFont=love.graphics.newFont(30)
 
@@ -31,27 +35,33 @@ end
 function love.update(dt)
 
     if gameState==2 then
+        local moveSpeed = player.speed
+
+        if player.injured then
+            moveSpeed = player.injuredSpeed
+        end
+
         if love.keyboard.isDown("d") then
             if player.x<screenWidth then
-                player.x = player.x + player.speed*dt
+                player.x = player.x + moveSpeed*dt
             end
         end
 
         if love.keyboard.isDown("a") then
             if player.x>0 then
-                player.x = player.x - player.speed*dt
+                player.x = player.x - moveSpeed*dt
             end
         end
 
         if love.keyboard.isDown("w") then
             if player.y > 0 then
-                player.y = player.y - player.speed*dt
+                player.y = player.y - moveSpeed*dt
             end
         end
 
         if love.keyboard.isDown("s") then
             if player.y < screenHeight then
-                player.y = player.y + player.speed*dt
+                player.y = player.y + moveSpeed*dt
             end
         end
     end
@@ -61,19 +71,28 @@ function love.update(dt)
         z.y = z.y + (math.sin(playerZombieAngle(z))*z.speed*dt)
 
         if distanceBetween(z.x,z.y,player.x,player.y)<30 then
-            for i,z in ipairs(zombies) do
-                zombies[i]=nil
+                        -- if the player is not injured, set injured to true
+            -- and also set the zombie that touched him to 'dead'
+            if player.injured == false then
+                player.injured = true
+                z.dead = true
+            -- otherwise, if the player was injured on collision,
+            -- destroy all zombies and go back to gameState 1
+            else
+                for i,z in ipairs(zombies) do
+                    zombies[i] = nil
+                    gameState = 1
+                    player.injured = false
+                    player.x = love.graphics.getWidth()/2
+                    player.y = love.graphics.getHeight()/2
+                end
             end
-
-            if highscore<score then
-                highscore=score
-            end
-
-            gameState=1
-            player.x=love.graphics.getWidth()/2
-            player.y=love.graphics.getHeight()/2
         end
+
     end
+
+
+
 
     for i,b in ipairs(bullets) do
         b.x = b.x + (math.cos(b.direction)*b.speed*dt)
@@ -127,7 +146,14 @@ end
 function love.draw()
     love.graphics.setFont(myFont)
     love.graphics.draw(sprites.background,0,0)
+    
+    if player.injured==true then
+        love.graphics.setColor(1,0,0)
+    end
+    
     love.graphics.draw(sprites.player,player.x ,player.y,playerMouseAngle(),nil,nil, sprites.player:getWidth()/2,sprites.player:getHeight()/2)
+
+    love.graphics.setColor(1,1,1)
     love.graphics.printf("score:"..score,0,love.graphics.getHeight()-100,love.graphics.getWidth(),"center")
     love.graphics.printf("Highscore:"..highscore,0,10,love.graphics.getWidth(),"center")
 
